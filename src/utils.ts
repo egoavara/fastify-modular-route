f998f24f539420ad71b79477ca94bc686d4e481c
김성현 <egoavara@gmail.com>
2022-04-26 13:25:41 +0900

    FIX, ParseRouteKeys

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,12 @@
 export type ParseRouteKeys<Route extends string> =
-    Route extends `/${string}:${infer K}/${infer LEFT}`
-    ? K | ParseRouteKeys<LEFT>
-    : Route extends `/${string}:${infer K}`
+    Route extends `/${infer POST}`
+    ? ParseRouteKeys<POST>
+    : Route extends `${string}/:${infer K}/${infer POST}`
+    ? K | ParseRouteKeys<POST>
+    : Route extends `:${infer K}/${infer POST}`
+    ? K | ParseRouteKeys<POST>
+    : Route extends `${string}/:${infer K}`
+    ? K
+    : Route extends `:${infer K}`
     ? K
     : never
